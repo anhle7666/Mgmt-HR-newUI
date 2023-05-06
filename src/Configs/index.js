8ac9b31d8662c748214d23c62a11d9af6f975c8f
anhle7666 <anhle7666@gmail.com>
2023-05-06 13:06:06 +0700

    lastest update

--- src/Configs/index.js
+++ src/Configs/index.js
@@ -1,6 +1,6 @@
 const backend = {
-    API_url: "http://localhost:8080/api",
-    // API_url: "https://newapi-o6mq.onrender.com/api",
+    // API_url: "http://localhost:8080/api",
+    API_url: "https://newapi-o6mq.onrender.com/api",
 };
 
 export default backend;
